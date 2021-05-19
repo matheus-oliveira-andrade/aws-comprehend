2e6ef976c94b81dc981e10b4c79536ec0848a9b7
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-05-18 21:05:03 -0300

    Initial

---
+++ aws-comprehend/FrmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace aws_comprehend
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtBox);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(649, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(138, 58);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(13, 12);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(630, 58);
+            this.txtBox.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 85);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(799, 527);
+            this.panel2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(799, 527);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 612);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ aws-comprehend/FrmMain.cs
@@ -0,0 +1,84 @@
+﻿using Amazon;
+using Amazon.Comprehend;
+using Amazon.Comprehend.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aws_comprehend
+{
+    public partial class FrmMain : Form
+    {
+        private const string AWS_PROFILE_NAME = "AWS Educate profile";
+        private const string AWS_ARN_COMPREHEND_CLASSIFIER = "arn:aws:comprehend:us-east-1:717510493583:document-classifier-endpoint/aws-comprehend";
+        private readonly RegionEndpoint _region = RegionEndpoint.USEast1;
+
+        private AWSCredentials _awsCredentials;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Get credentials in machine
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+
+            if (!chain.TryGetAWSCredentials(AWS_PROFILE_NAME, out _awsCredentials))
+            {
+                MessageBox.Show("Error on get credentials");
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            //await SendTextToAwsComprehendDectectSentiment();
+
+            await SendTextToAwsComprehendCustomClassify();
+        }
+
+        private async Task SendTextToAwsComprehendCustomClassify()
+        {
+            GetCredentials();
+
+            var client = new AmazonComprehendClient(_awsCredentials, _region);
+
+            var request = new ClassifyDocumentRequest
+            {
+                EndpointArn = AWS_ARN_COMPREHEND_CLASSIFIER,
+                Text = txtBox.Text,
+            };
+
+            ClassifyDocumentResponse response = await client.ClassifyDocumentAsync(request);
+
+            string json = JsonConvert.SerializeObject(response, Formatting.Indented);
+
+            richTextBox1.Clear();
+            richTextBox1.AppendText(json);
+        }
+
+        private async Task SendTextToAwsComprehendDectectSentiment()
+        {
+            GetCredentials();
+
+            var client = new AmazonComprehendClient(_awsCredentials, _region);
+
+            var request = new DetectSentimentRequest
+            {
+                Text = txtBox.Text,
+                LanguageCode = LanguageCode.Pt,
+            };
+
+            DetectSentimentResponse response = await client.DetectSentimentAsync(request);
+
+            string json = JsonConvert.SerializeObject(response, Formatting.Indented);
+
+            richTextBox1.Clear();
+            richTextBox1.AppendText(json);
+        }
+    }
+}
